76918af9767bf7abef6ec7ee2228ca3540ec737a
juanarrieta1986 <juanca86@gmail.com>
2021-03-19 04:32:07 +0000

    v3

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,5 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	const apiUrl = "https://swapi.py4e.com/api/";
 	return {
 		store: {
 			demo: [
@@ -12,17 +13,66 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			planets: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
+			loadPlanets: () => {
+				var myHeaders = new Headers();
+				myHeaders.append("Cookie", "__cfduid=d3e5242c92f8f533f884f0407f34f004a1616125125");
+				//myHeaders.append("Host", "<calculated when request is sent>");
+
+				//var raw = "";
+
+				var requestOptions = {
+					method: "GET",
+					//mode: "no-cors",
+					headers: myHeaders,
+					//body: raw,
+					redirect: "follow"
+				};
+
+				fetch(apiUrl + "planets", requestOptions)
+					.then(response => response.json())
+					//.then(result => console.log(result.results))
+					.then(result => setStore({ planets: result.results }))
+					.catch(error => console.log("error", error));
+				//setStore({ planets: fillPlanets });
+				//console.log(store.planets);
+			},
+			loadCharacters: () => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+                */
+
+				var requestOptions = {
+					method: "GET",
+					redirect: "follow"
+				};
+
+				fetch(apiUrl + "people", requestOptions)
+					.then(response => response.text())
+					.then(result => console.log(result))
+					.catch(error => console.log("error", error));
+			},
+			loadStarships: () => {
+				/**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+                */
+
+				var requestOptions = {
+					method: "GET",
+					redirect: "follow"
+				};
+
+				fetch(apiUrl + "starships", requestOptions)
+					.then(response => response.text())
+					.then(result => console.log(result))
+					.catch(error => console.log("error", error));
 			},
 			changeColor: (index, color) => {
 				//get the store
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../../styles/home.scss";
 import Card from "../component/card";
 
+import { Context } from "../store/appContext";
+
 export const Home = () => {
-	let planets = [
+	const { store, actions } = useContext(Context);
+	//const store = getStore();
+	//console.log(store.planets);
+	/*let planets = [
 		{ name: "mercurio", surface: 1000 },
 		{ name: "venus", surface: 3000 },
 		{ name: "tierra", surface: 5000 },
@@ -22,13 +27,13 @@ export const Home = () => {
 		{ name: "mercurio", surface: 1000 },
 		{ name: "venus", surface: 3000 },
 		{ name: "tierra", surface: 5000 }
-	];
+	];*/
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Home</h1>
 			<div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
-				{planets.map((item, index) => {
+				{store.planets.map((item, index) => {
 					return (
 						<div className="col" key={index}>
 							<Card key={index} name={item.name} surface={item.surface} />
